f200c132e0a4695864b88f4b4726f09cbbd160a4
Rishav1167 <Rishav1167>
2025-06-07 15:54:29 +0530

    [Rishav] feat: implement employee APIs for fetching all, by ID, and by hire date

--- src/main/java/com/hr_management/hr_management/controller/EmployeeController.java
+++ src/main/java/com/hr_management/hr_management/controller/EmployeeController.java
@@ -2,17 +2,21 @@ package com.hr_management.hr_management.controller;
 
 import com.hr_management.hr_management.mapper.EmployeeMapper;
 import com.hr_management.hr_management.model.dto.CountryDTO;
+import com.hr_management.hr_management.model.dto.EmployeeDTO;
 import com.hr_management.hr_management.model.dto.LocationDTO;
 import com.hr_management.hr_management.model.entity.Countries;
 import com.hr_management.hr_management.model.entity.Employee;
 import com.hr_management.hr_management.repository.EmployeeRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -26,12 +30,27 @@ public class EmployeeController {
         this.employeeMapper = employeeMapper;
     }
 
+    // Get all employees
     @GetMapping("employees")
-    public List<Employee> getAllEmployees() {
-        /*return employeeRepository.findAll().stream()
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeRepository.findAll().stream()
                 .map(employeeMapper::toDTO)
-                .collect(Collectors.toList());*/
-        return employeeRepository.findAll().stream().toList();
-    }
+                .collect(Collectors.toList());
 
+    }
+    // Get employees by hire date
+    @GetMapping("employees/{hireDate}")
+    public List<EmployeeDTO> getEmployeeByHireDate(
+            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hireDate) {
+        return employeeRepository.findAllByHireDate(hireDate).stream()
+                .map(employeeMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+    // Get employee by ID
+    @GetMapping("employees/id/{employee_id}")
+    public EmployeeDTO getEmployeeById(@PathVariable BigDecimal employee_id) {
+        return employeeMapper.toDTO(
+                employeeRepository.readEmployeeByEmployeeId(employee_id));
+
+    }
 }
--- src/main/java/com/hr_management/hr_management/repository/EmployeeRepository.java
+++ src/main/java/com/hr_management/hr_management/repository/EmployeeRepository.java
@@ -1,11 +1,18 @@
 package com.hr_management.hr_management.repository;
 
+import com.hr_management.hr_management.model.dto.EmployeeDTO;
 import com.hr_management.hr_management.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @RepositoryRestResource(path = "employee")
 public interface EmployeeRepository extends JpaRepository<Employee, BigDecimal> {
+    List<Employee> findAllByHireDate(LocalDate hireDate);
+
+    Employee readEmployeeByEmployeeId(BigDecimal employeeId);
 }
